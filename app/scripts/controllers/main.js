36422e31d1dcc419663dd56f405aeaee34dd7578
Lee Walters <lee@leewalters.net>
2016-08-17 11:51:10 +0100

    yeoman scaffold angular app

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name flickYerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the flickYerApp
+ */
+angular.module('flickYerApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
