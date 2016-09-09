0f3916a70c4ec93d265c77df75b87e037b8f058a
Lee Walters <lee@leewalters.net>
2016-09-09 17:47:17 +0100

    removed templates as URLS. Upgraded directive to ng 1.5 component

--- app/scripts/pto-social-common/directives/flickrfeeduidetail.js
+++ app/scripts/pto-social-common/directives/flickrfeeduidetail.js
@@ -9,7 +9,20 @@
 angular.module('pto.social.common')
   .directive('flickrFeedUiDetail', function ($log, TweenMax) {
     return {
-      templateUrl: 'views/pto-social-common/flickrFeedUIDetail.html',
+      template: `
+          <section>
+            <button ng-click="close()" class="close">Back</button>
+            <a href="{{model.link}}" target="_blank"><h2>{{model.title}}</h2></a>
+            <div class="pto-social_flickr-image_detail" ng-style="{'background-image': 'url(' + model.image + ')'}"></div>
+            <small><a ng-href="https://www.flickr.com/photos/{{model.authorId}}" target="_blank">{{model.authorName}}</a></small> | 
+            <small>Published {{model.formattedDate}} at {{model.formattedTime}}</small>
+            <p>Bacon ipsum dolor amet pancetta tongue short ribs beef shoulder bacon shank sausage short loin landjaeger bresaola brisket. Swine cow boudin, hamburger shoulder kevin prosciutto turducken meatball chicken. Sirloin pork chop turducken pig strip steak meatball cow salami kielbasa kevin capicola frankfurter. Tail chuck hamburger beef prosciutto leberkas kevin bacon pork belly sirloin rump pork. Landjaeger corned beef cupim short loin beef ribs, tail t-bone turkey fatback salami ribeye kielbasa turducken. Porchetta jerky chuck brisket, tongue boudin ham hamburger rump pork belly cupim pork short ribs beef ribs.</p><p>Flank ground round chuck pork loin fatback andouille boudin ball tip venison pig. Chuck fatback tri-tip brisket kielbasa tail. Chuck ham hock brisket turducken t-bone jowl. Tongue swine chicken kevin, pancetta alcatra pork loin pastrami shank venison capicola ground round. Jerky venison t-bone capicola fatback porchetta turducken strip steak. Pastrami shoulder rump, pork chop tenderloin turducken bacon landjaeger capicola meatloaf strip steak. Porchetta fatback alcatra sausage hamburger boudin venison frankfurter biltong andouille shoulder pork belly.</p>
+            <small>tags: </small>
+            <ul>
+                <li ng-repeat="tag in model.tags track by $index" ng-click="$ctrl.selectTag(tag)">{{tag}}</li>
+            </ul>
+          </section>
+        `,
       restrict: 'E',
       scope: {
         model: '=ngModel',
--- app/scripts/pto-social-common/directives/flickrfeeduilist.js
+++ app/scripts/pto-social-common/directives/flickrfeeduilist.js
@@ -7,71 +7,87 @@
  * # flickrFeedUIList
  */
 angular.module('pto.social.common')
-  .directive('flickrFeedUiList', function ($timeout, flickrFeedService) {
-    return {
-      templateUrl: 'views/pto-social-common/flickrFeedUIList.html',
-      restrict: 'EA',
-      scope: {
-        tags: '=',
-        reveal: '='
-      },
-      controllerAs: 'flkrUiListCtlr',
-      controller: function ($scope) {
-        var vm = this;
-        vm.showDetail = false;
-        vm.feedListFormatted = [];
-        vm.selectedDetail = {};
-        vm.loadingFlickrFeed = true;
-        
-        $timeout(function() {
-          vm.tags = $scope.tags;
-          vm.getFeed();
-        });
+  .component('flickrFeedUiList', {
+    template: `
+      <p ng-if="$ctrl.loadingFlickrFeed">Loading feed...</p>
+      <section class="pto-social_flickr-feed-container" ng-show="!$ctrl.showDetail">
+          <article class="pto-social_flickr-feed-row" ng-repeat="item in $ctrl.feedListFormatted track by $index">
+              <div class="pto-social_flickr-image" ng-click="$ctrl.gotoDetail(item)" ng-style="{'background-image': 'url(' + item.image + ')'}"></div>
+              <div class="pto-social_flickr-info">
+                  <h2 ng-click="$ctrl.gotoDetail(item)">{{item.title}}</h2>
+                  <small class="hideOnDesktop">Published {{item.formattedDate}} at {{item.formattedTime}}<br></small>
+                  <small><a ng-href="https://www.flickr.com/photos/{{item.authorId}}" target="_blank">{{item.authorName}}</a></small>
+                  <small class="hideOnTablet">Published {{item.formattedDate}} at {{item.formattedTime}}</small>
+                  <small><a href="{{item.link}}" target="_blank">View on Flickr</a></small>
+              </div>
+          </article>
+      </section>
+      <flickr-feed-ui-detail class="pto-social_flickr-detail" reveal="$ctrl.showDetail" ng-model="$ctrl.selectedDetail" close="$ctrl.closeDetail()"></flickr-feed-ui-detail>
+    `,
+    restrict: 'EA',
+    bindings: {
+      tags: '=',
+      reveal: '='
+    },
+    controller: function ($scope, $timeout, flickrFeedService) {
+      var vm = this;
+      vm.showDetail = false;
+      vm.feedListFormatted = [];
+      vm.selectedDetail = {};
+      vm.loadingFlickrFeed = true;
+      vm.tags = this.tags;
+      
+      $timeout(function() {
+        vm.getFeed();
+      });
 
-        vm.getFeed = function () {
+      vm.getFeed = function () {
+        vm.apiFailure = false;
+        return flickrFeedService.getFeed(vm.tags.toString()).then(function(data) {
+          vm.feedList = data;
+          vm.loadingFlickrFeed = false;
+          reformatList();
           vm.apiFailure = false;
-          return flickrFeedService.getFeed(vm.tags.toString()).then(function(data) {
-            vm.feedList = data;
-            vm.loadingFlickrFeed = false;
-            reformatList();
-            vm.apiFailure = false;
-          }, function () {
-            vm.apiFailure = true;
-            vm.loadingFlickrFeed = false;
-          }).catch(function() {
-            vm.apiFailure = true;
-            vm.loadingFlickrFeed = false;
-          });
-        };
+        }, function () {
+          vm.apiFailure = true;
+          vm.loadingFlickrFeed = false;
+        }).catch(function() {
+          vm.apiFailure = true;
+          vm.loadingFlickrFeed = false;
+        });
+      };
 
-        function reformatList () {
-          for (var index = 0; index < vm.feedList.length; index++) {
-            var element = vm.feedList[index];
-            var newItem = {
-              formattedDate: flickrFeedService.getFormattedDate(element.published),
-              formattedTime: flickrFeedService.getFormattedTime(element.published),
-              authorName: flickrFeedService.getAuthorName(element.author),
-              /* jshint ignore:start */
-              authorId: element.author_id,
-              /* jshint ignore:end */
-              title: element.title,
-              link: element.link,
-              image: element.media.m,
-              tags: element.tags.split(' ')
-            };
-            
-            vm.feedListFormatted[index] = newItem;
-          }
+      function reformatList () {
+        for (var index = 0; index < vm.feedList.length; index++) {
+          var element = vm.feedList[index];
+          var newItem = {
+            formattedDate: flickrFeedService.getFormattedDate(element.published),
+            formattedTime: flickrFeedService.getFormattedTime(element.published),
+            authorName: flickrFeedService.getAuthorName(element.author),
+            /* jshint ignore:start */
+            authorId: element.author_id,
+            /* jshint ignore:end */
+            title: element.title,
+            link: element.link,
+            image: element.media.m,
+            tags: element.tags.split(' ')
+          };
+          
+          vm.feedListFormatted[index] = newItem;
         }
-
-        vm.gotoDetail = function (item) {
-          vm.showDetail = true;
-          vm.selectedDetail = item;
-        };
-
-        vm.closeDetail = function () {
-          vm.showDetail = false;
-        };
       }
-    };
+
+      vm.gotoDetail = function (item) {
+        vm.showDetail = true;
+        vm.selectedDetail = item;
+      };
+
+      vm.closeDetail = function () {
+        vm.showDetail = false;
+      };
+
+      this.$postLink = function () {
+
+      };
+    }
   });
